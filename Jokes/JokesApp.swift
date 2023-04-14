bc718f1158c901d2cfcd64d14ce826f60d02b061
Morgan <Mharrisstoertz@gmail.com>
2023-04-14 14:35:31 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Morgan Harris-Stoertz on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
