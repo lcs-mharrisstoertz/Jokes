d44ccf21431f84a58aab969c507961055e5787a8
Morgan <Mharrisstoertz@gmail.com>
2023-04-14 14:45:11 -0400

    Added button placeholder and view to show the punchline.

--- Jokes/Views/JokeView.swift
+++ Jokes/Views/JokeView.swift
@@ -15,6 +15,20 @@ struct JokeView: View {
                     .font(.title)
                     .multilineTextAlignment(.center)
                 
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "arrow.down.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40)
+                        .tint(.black)
+                })
+                
+                Text("They are hill areas.")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                
             }
             .navigationTitle("Random Jokes")
         }
